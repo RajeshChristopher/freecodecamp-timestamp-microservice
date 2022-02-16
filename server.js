9e4c53a71e90fa018b8340352c0f8885f2ac8d1d
RajeshChristopher <47190431+RajeshChristopher@users.noreply.github.com>
2022-02-17 03:09:39 +0530

    timestamp microservice solu

--- server.js
+++ server.js
@@ -24,6 +24,57 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/",function(req,res){
+  var a = new Date().getTime();
+  var b = new Date().toUTCString();
+  res.json({unix:a, utc:b});
+});
+
+app.get("/api/:date?",function(req,res){
+  var d = req.params.date;
+  console.log(d);
+  //var e = d
+  //console.log(e);
+  
+  //console.log(x);
+    var x = req.params.date.split("");
+    console.log(x);
+    if(x.includes('-')){
+    var c = new Date(d).toUTCString();
+    var a = new Date(d).getTime();
+    console.log(c);
+    console.log(a);
+
+    if(c == "Invalid Date"){
+      res.json({error: "Invalid Date"});
+    }
+    
+    res.json({unix: parseInt(a), utc: c});
+  }else if(x.includes(" ")){
+    //console.log(d);
+    var z = Date.parse(d);
+    //console.log(z);
+    if(z == "Invalid Date"){
+    res.json({error: "Invalid Date"});
+  }
+    var t = new Date(z).toUTCString();
+    //console.log(t);
+    res.json({unix:z, utc: t})
+  }
+  else{
+    //console.log(d);
+  var y = parseInt(d);
+  //console.log(y);
+  var c = new Date(y).toUTCString();
+  //console.log(c);
+  if(y == "Invalid Date" || c =="Invalid Date"){
+    res.json({error: "Invalid Date"});
+  }else{
+    res.json({unix: y, utc: c});
+  }
+  
+  }
+});
 
 
 // listen for requests :)
